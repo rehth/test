9204deb6a0b0215675158a4caba4687619574594
张千军 <1515171493@qq.co>
2017-11-12 16:19:41 +0800

    测试应用逻辑

--- new_project/new/views.py
+++ new_project/new/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def test_app(request):
+    # 测试应用逻辑
+    return render(request, 'new/test_app.html')
